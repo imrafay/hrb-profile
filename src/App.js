a5eeb951b5a005f25bf69e79fca12e9ee031c7f8
Hassan Rafay <h.baig@keee.net>
2024-06-05 19:18:55 +0200

    Created under-construction page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Logo from './Assets/HRB-Logo.png'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="animate-pulse flex flex-col items-center justify-center h-screen bg-gray-200">
+      <div className="mb-8">
+        <img
+          src={Logo}
+          alt="Under Construction"
+          className="w-58 h-48"
+        />
+      </div>
+      <h1 className="text-2xl font-bold text-gray-800">
+        This page is under construction.
+      </h1>
+      <p className="text-md text-gray-600 mt-4">
+        We're working hard to bring you something amazing. Stay tuned!
+      </p>
     </div>
   );
 }
