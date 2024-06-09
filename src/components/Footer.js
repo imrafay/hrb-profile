4185503099afc189ae49fe76bd11215af5d761ad
Hassan Rafay <h.baig@keee.net>
2024-06-09 23:38:40 +0200

    Added a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 w-full p-4 text-white text-center">
+    <footer className="fixed bottom-0 w-full p-4 text-center bg-slate-300 text-gray-700">
     <p>&copy; Designed and Developed by Hassan Rafay Baig.</p>
     <p>Built with ReactJS & Tailwind CSS. Hosted on Netlify.</p>
   </footer>
