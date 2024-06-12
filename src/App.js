f6c74b76e1667f64418e6ae5d22ec0ae9810b40a
Hassan Rafay <h.baig@keee.net>
2024-06-12 04:42:57 +0200

    Added header, footer and banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer';
-import Banner from './components/Banner';
+import Banner from './sections/Banner';
+import Header from './sections/Header';
+import Footer from './sections/Footer';
 
 
 function App() {
