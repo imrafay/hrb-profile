a232ba4821ce41552cfb24be07b41ef465a9c5c2
Hassan Rafay <h.baig@keee.net>
2024-07-14 18:44:06 +0200

    Added resume

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -20,11 +20,22 @@ function Header() {
       <img src={Logo} alt="Logo" style={{width:'250px'}}/>
       
       <ul className="hidden md:flex">
-        <li><Link to="home" smooth={true} duration={500} >Home</Link></li>
-        <li><Link to="about" smooth={true} duration={500} >About</Link></li>
-        <li><Link to="skills" smooth={true} duration={500} >Skills</Link></li>
-        <li><Link to="projects" smooth={true} duration={500} >Projects</Link></li>
-        <li>Resume</li>
+        <li className="py-2"><Link to="home" smooth={true} duration={500} >Home</Link></li>
+        <li className="py-2"><Link to="about" smooth={true} duration={500} >About</Link></li>
+        <li className="py-2"><Link to="skills" smooth={true} duration={500} >Skills</Link></li>
+        <li className="py-2"><Link to="projects" smooth={true} duration={500} >Projects</Link></li>
+        <li>
+        <a 
+        href={`${process.env.PUBLIC_URL}/Resume.pdf`} 
+        download
+        className="inline-flex items-center px-5 py-2 text-white bg-gray-900 rounded-lg hover:bg-gray-600"
+      >
+        Resume
+        <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v16h16V4H4zm2 4h12m-6 4v4m-4-4h8"></path>
+        </svg>
+      </a>
+        </li>
       </ul>
 
     <div onClick={handleClickNav} className={mobileNav?'hidden': 'md:hidden cursor-pointer z-10'}>
